35184166d38c10a3ddee4bc18838e14f6e7d5a84
cGuillaumec <115501940+cGuillaumec@users.noreply.github.com>
2022-10-10 23:19:13 +0200

    Update app.r

--- app.r
+++ app.r
@@ -1,7 +1,7 @@
 
 #GROUP 6: Dalia BOUDJEMIA, Guillaume CAMPAN, Annaëlle GUEZ, Mohamed-Amine
 
-HP <- readr::read_csv("https://github.com/cGuillaumec/ProjectRstudioGroup6/blob/f0504d50fe7c7ffadbdf132e52fd3aeaab4bd23c/DP_LIVE_03102022234015026.csv")
+HP <- readr::read_csv("https://github.com/cGuillaumec/ProjectRstudioGroup6/blob/3e6b20cb8493f06e3374b7f9c0f7d4a5184f0d92/DATA/DP_LIVE_03102022234015026.csv")
 library(data.table)
 nrow(HP)
 setDT(HP)   
